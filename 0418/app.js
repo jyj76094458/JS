0fb2f6be8e3560f40896082f02703b83a54c2309
jyj76094458 <164857781+jyj76094458@users.noreply.github.com>
2024-05-01 15:17:43 +0900

    0418

--- 0418/app.js
+++ 0418/app.js
@@ -15,3 +15,18 @@ app.get('/main3', function(req, res){
   res.sendfile("main3.html");
 });
 
+app.get('/main4', function(req, res){
+  res.sendfile("main4.html");
+});
+
+app.get('/main5', function(req, res){
+  res.sendfile("main5.html");
+});
+
+app.get('/main6', function(req, res){
+  res.sendfile("main6.html");
+});
+
+app.get('/main7', function(req, res){
+  res.sendfile("main7.html");
+});
